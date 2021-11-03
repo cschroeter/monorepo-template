f10c0722f347004afdb832263a36b8dfa30d40c8
Christian Schröter <dev@cschroeter.net>
2021-11-03 13:31:34 +0100

    feat: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  stories: ['../packages/**/*.stories.tsx'],
+  addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: { backgrounds: false },
+    },
+  ],
+  typescript: {
+    reactDocgen: false,
+  },
+}
---
+++ packages/website/stories/Demo.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default {
+  title: 'Demo',
+}
+
+export const MyDemo = () => (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+)
