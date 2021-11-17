a2c6714eb8bae65312b34b81bc6528842184b961
Christian Schröter <dev@cschroeter.net>
2021-11-17 09:33:00 +0100

    chore: wip

--- packages/ui/src/Button.stories.tsx
+++ packages/ui/src/Button.stories.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
 import { Button } from './Button'
+
+import React, { useState, useEffect } from 'react'
 
 export default {
   title: 'My Button Story',
 }
 
 export const Demo = () => {
-  const [state, setstate] = useState(0)
+  const [state, setstate] = useState(1)
 
   return <Button onClick={() => setstate(state + 1)}>{state.toString()}</Button>
 }
