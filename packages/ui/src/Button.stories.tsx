6a015fa4561fee712830c9e9f51fb2f0076e12e2
Christian Schröter <dev@cschroeter.net>
2021-11-18 07:58:55 +0100

    chore: add icon button

--- packages/ui/src/Button.stories.tsx
+++ packages/ui/src/Button.stories.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import { Button } from './Button'
+import { IconButton } from './IconButton'
 
 export default {
   title: 'My Button Story',
 }
 
-export const Demo = () => {
+export const MyButton = () => {
   const [state, setstate] = useState(0)
 
   return <Button onClick={() => setstate(state + 1)}>{state.toString()}</Button>
 }
+
+export const MyIconButton = () => <IconButton />
---
+++ packages/ui/src/IconButton.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Button } from './Button'
+
+export const IconButton = () => {
+  return <Button>With Icon</Button>
+}
