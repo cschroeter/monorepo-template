1db706e09c819665a9180d067688d572209fd9a5
Christian Schröter <dev@cschroeter.net>
2021-11-18 09:00:18 +0100

    chore: wip

--- packages/ui/src/IconButton.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Button } from './Button'
-
-export const IconButton = () => {
-  return <Button>With Icon</Button>
-}
--- packages/ui/src/MyComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react'
-import { Button } from './Button'
-
-export const MyComponent = () => {
-  const [count, setCount] = useState(0)
-  return <Button onClick={() => setCount(count + 1)}>{count}</Button>
-}
