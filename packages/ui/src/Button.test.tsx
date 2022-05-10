230ba1ce9e2e0cfa068c11e70064466086a63bc6
Christian Schröter <dev@cschroeter.net>
2022-05-10 10:32:11 +0200

    chore: apply new prettier setup

--- packages/ui/src/Button.test.tsx
+++ packages/ui/src/Button.test.tsx
@@ -1,19 +1,19 @@
-import { render } from '@testing-library/react'
-import * as React from 'react'
-import { Button } from './Button'
+import { render } from "@testing-library/react";
+import * as React from "react";
+import { Button } from "./Button";
 
 test.each([
-  'falsemjgntur',
-  'kzvoovko',
-  'umkt',
-  'gkjfzeu',
-  'grvtncgkekk',
-  'nkxpsxppmjg',
-  'owsdxmik',
-  'zocjozwq',
-  'vuswtg',
-  'cairoiexitzo',
-])('a button works as expected %s', async (value: string) => {
-  const screen = render(<Button>{value}</Button>)
-  expect(screen.getByText(value)).toBeInTheDocument()
-})
+  "falsemjgntur",
+  "kzvoovko",
+  "umkt",
+  "gkjfzeu",
+  "grvtncgkekk",
+  "nkxpsxppmjg",
+  "owsdxmik",
+  "zocjozwq",
+  "vuswtg",
+  "cairoiexitzo",
+])("a button works as expected %s", async (value: string) => {
+  const screen = render(<Button>{value}</Button>);
+  expect(screen.getByText(value)).toBeInTheDocument();
+});
