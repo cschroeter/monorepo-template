82277082dbf640daae58088540ba5151529bcbb3
Christian Schröter <dev@cschroeter.net>
2022-05-10 10:33:26 +0200

    chore: apply new prettier setup

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const withTM = require("next-transpile-modules")(["@template/ui"]);
+const withTM = require('next-transpile-modules')(['@template/ui'])
 
 module.exports = withTM({
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-});
+})
